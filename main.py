c8acaf8ed9d8dd348bcca023c171c3040af71d99
Dmytro <shepelev.dim@gmail.com>
2024-03-20 13:46:04 +0100

    Added students_dictionary and function add_to_student_dictionary

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+students_dictionary = {}
+
+def add_to_student_dictionary(id, first_name, last_name, age):
+    students_dictionary[id] = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age
+    }
+
+print(students_dictionary)
+
+add_to_student_dictionary(1, 'Dmytro', 'Shepeliev', 25)
+
+print(students_dictionary)
